7b6c4bc6e0581e35ccf4d1952a00ec324bdedd5c
Thrashy190 <72418715+Thrashy190@users.noreply.github.com>
2023-01-19 15:57:09 -0600

    finish user home designe

--- src/views/users/inicio/Inicio.js
+++ src/views/users/inicio/Inicio.js
@@ -1,7 +1,145 @@
 import React from 'react'
+import {
+  CCol,
+  CRow,
+  CContainer,
+  CCard,
+  CCardBody,
+  CCardTitle,
+  CCarousel,
+  CCarouselItem,
+  CCardSubtitle,
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+
+import {
+  cilMemory,
+  cilCode,
+  cilQrCode,
+  cilBarChart,
+  cilDescription,
+  cilBeaker,
+  cilBolt,
+  cilCarAlt,
+} from '@coreui/icons'
 
 const Inicio = () => {
-  return <div>Inicio</div>
+  const carreras = [
+    {
+      nombre: 'Sistemas computacionales',
+      icono: cilCode,
+      recomendaciones: [
+        { nombre: 'java', autor: 'Diego' },
+        { nombre: 'Matematicas', autor: 'Diego' },
+      ],
+    },
+    {
+      nombre: 'Mecatronica',
+      icono: cilQrCode,
+      recomendaciones: [
+        { nombre: 'java', autor: 'Diego' },
+        { nombre: 'Matematicas', autor: 'Diego' },
+      ],
+    },
+    {
+      nombre: 'Mecanica',
+      icono: cilCarAlt,
+      recomendaciones: [
+        { nombre: 'java', autor: 'Diego' },
+        { nombre: 'Matematicas', autor: 'Diego' },
+      ],
+    },
+    {
+      nombre: 'Electrica',
+      icono: cilBolt,
+      recomendaciones: [
+        { nombre: 'java', autor: 'Diego' },
+        { nombre: 'Matematicas', autor: 'Diego' },
+      ],
+    },
+    {
+      nombre: 'Electronica',
+      icono: cilMemory,
+      recomendaciones: [
+        { nombre: 'java', autor: 'Diego' },
+        { nombre: 'Matematicas', autor: 'Diego' },
+      ],
+    },
+    {
+      nombre: 'Industrial',
+      icono: cilDescription,
+      recomendaciones: [
+        { nombre: 'java', autor: 'Diego' },
+        { nombre: 'Matematicas', autor: 'Diego' },
+      ],
+    },
+    {
+      nombre: 'Gestion Empresarial',
+      icono: cilBarChart,
+      recomendaciones: [
+        { nombre: 'java', autor: 'Diego' },
+        { nombre: 'Matematicas', autor: 'Diego' },
+      ],
+    },
+    {
+      nombre: 'Material',
+      icono: cilBeaker,
+      recomendaciones: [
+        { nombre: 'java', autor: 'Diego' },
+        { nombre: 'Matematicas', autor: 'Diego' },
+      ],
+    },
+  ]
+
+  return (
+    <>
+      <CContainer>
+        <CRow className="pb-5">
+          <CCol>
+            <h1>!Bienvenido¡</h1>
+          </CCol>
+        </CRow>
+        <CRow className="pb-4 text-medium-emphasis">
+          <CCol>
+            <h2>Recomendados</h2>
+          </CCol>
+        </CRow>
+
+        {carreras.map(({ nombre, icono, recomendaciones }) => {
+          return (
+            <CRow key={this} className="pb-4">
+              <CCol xs={3}>
+                <CCard style={{ width: '18rem' }}>
+                  <CCardBody className="d-flex flex-column align-items-center">
+                    <CIcon icon={icono} size={'8xl'} />
+                    <CCardTitle className="pt-4">{nombre}</CCardTitle>
+                  </CCardBody>
+                </CCard>
+              </CCol>
+              <CCol xs={9}>
+                <CCarousel controls indicators dark>
+                  {recomendaciones.map(({ nombre, autor }) => {
+                    return (
+                      <CCarouselItem key={this}>
+                        <CCard style={{ width: '100%', height: '13.50rem' }}>
+                          <CCardBody className="d-flex flex-column align-items-center">
+                            <CCardTitle className="pt-4 fs-1">{nombre}</CCardTitle>
+                            <CCardSubtitle className="pt-4 fs-3 text-medium-emphasis">
+                              {autor}
+                            </CCardSubtitle>
+                          </CCardBody>
+                        </CCard>
+                      </CCarouselItem>
+                    )
+                  })}
+                </CCarousel>
+              </CCol>
+            </CRow>
+          )
+        })}
+      </CContainer>
+    </>
+  )
 }
 
 export default Inicio
