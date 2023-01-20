fa4308a8142cc2c0d4abbe39e254d011385e15b4
Thrashy190 <72418715+Thrashy190@users.noreply.github.com>
2023-01-20 14:02:08 -0600

    add search bar to header

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { CContainer, CHeader, CHeaderDivider, CHeaderToggler } from '@coreui/react'
+import {
+  CContainer,
+  CHeader,
+  CHeaderDivider,
+  CHeaderToggler,
+  CFormInput,
+  CFormSelect,
+  CButton,
+} from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 import { cilMenu } from '@coreui/icons'
 
@@ -19,6 +27,23 @@ const AppHeader = () => {
         >
           <CIcon icon={cilMenu} size="lg" />
         </CHeaderToggler>
+        <div className="d-flex flex-row d-grid gap-4">
+          <CFormInput
+            style={{ width: '30rem' }}
+            type="email"
+            id="floatingInput"
+            floatingLabel="¿Qué estas buscando?"
+            placeholder="¿Qué estas buscando?"
+          />
+          <CFormSelect style={{ width: '10rem' }} aria-label="Default select example">
+            <option value="1">Libros</option>
+            <option value="2">Tesis</option>
+            <option value="3">Proyectos de residencias</option>
+          </CFormSelect>
+          <CButton color="dark" style={{ width: '10rem' }}>
+            Buscar
+          </CButton>
+        </div>
       </CContainer>
       <CHeaderDivider />
       <CContainer fluid>
