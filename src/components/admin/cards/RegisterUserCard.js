ad84c873800e82d6795a582dd57cdfb6b29da6d1
Thrashy190 <diego456.dlm77@gmail.com>
2023-04-14 16:30:59 -0600

    finish add user views in admin page and fix main search bar bug and responsive

---
+++ src/components/admin/cards/RegisterUserCard.js
@@ -0,0 +1,152 @@
+import React, { useState } from 'react'
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCardTitle,
+  CCol,
+  CForm,
+  CFormInput,
+  CFormLabel,
+  CRow,
+} from '@coreui/react'
+
+const RegisterUserCard = () => {
+  const [formValues, setFormValues] = useState({
+    firstName: '',
+    lastName: '',
+    middleName: '',
+    email: '',
+    birthYear: '',
+    password: '',
+    employeeId: '',
+  })
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+    setFormValues({ ...formValues, [name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // Here you can implement your logic to submit the form values
+    console.log(formValues)
+    setFormValues({
+      firstName: '',
+      lastName: '',
+      middleName: '',
+      email: '',
+      birthYear: '',
+      password: '',
+      employeeId: '',
+    })
+  }
+
+  const handleCancel = () => {
+    setFormValues({
+      firstName: '',
+      lastName: '',
+      middleName: '',
+      email: '',
+      birthYear: '',
+      password: '',
+      employeeId: '',
+    })
+  }
+
+  return (
+    <CCard>
+      <CCardHeader>
+        <CCardTitle className="pt-2">Agregar Usuario</CCardTitle>
+      </CCardHeader>
+      <CCardBody>
+        <CForm onSubmit={handleSubmit}>
+          <CRow>
+            <CCol xs={12} md={4}>
+              <CFormLabel>Nombre</CFormLabel>
+              <CFormInput
+                type="text"
+                name="firstName"
+                value={formValues.firstName}
+                onChange={handleInputChange}
+              />
+            </CCol>
+            <CCol xs={12} md={4}>
+              <CFormLabel>Apellido Paterno</CFormLabel>
+              <CFormInput
+                type="text"
+                name="lastName"
+                value={formValues.lastName}
+                onChange={handleInputChange}
+              />
+            </CCol>
+            <CCol xs={12} md={4}>
+              <CFormLabel>Apellido Materno</CFormLabel>
+              <CFormInput
+                type="text"
+                name="middleName"
+                value={formValues.middleName}
+                onChange={handleInputChange}
+              />
+            </CCol>
+          </CRow>
+
+          <CRow>
+            <CCol xs={12} md={4}>
+              <CFormLabel>Año de Nacimiento</CFormLabel>
+              <CFormInput
+                type="number"
+                name="birthYear"
+                value={formValues.birthYear}
+                onChange={handleInputChange}
+              />
+            </CCol>
+
+            <CCol xs={12} md={4}>
+              <CFormLabel>Matrícula de Empleado</CFormLabel>
+              <CFormInput
+                type="text"
+                name="employeeId"
+                value={formValues.employeeId}
+                onChange={handleInputChange}
+              />
+            </CCol>
+          </CRow>
+          <CRow>
+            <CCol xs={12} md={4}>
+              <CFormLabel>Correo</CFormLabel>
+              <CFormInput
+                type="email"
+                name="email"
+                value={formValues.email}
+                onChange={handleInputChange}
+              />
+            </CCol>
+            <CCol xs={12} md={4}>
+              <CFormLabel>Contraseña</CFormLabel>
+              <CFormInput
+                type="password"
+                name="password"
+                value={formValues.password}
+                onChange={handleInputChange}
+              />
+            </CCol>
+          </CRow>
+          <CRow className="pt-4 ">
+            <CCol className="d-flex justify-content-end gap-4">
+              <CButton type="button" color="secondary" onClick={handleCancel}>
+                Cancelar
+              </CButton>
+              <CButton type="submit" color="primary" className="mr-2">
+                Crear
+              </CButton>
+            </CCol>
+          </CRow>
+        </CForm>
+      </CCardBody>
+    </CCard>
+  )
+}
+
+export default RegisterUserCard
--- src/views/admin/usuarios/Usuarios.js
+++ src/views/admin/usuarios/Usuarios.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { CContainer } from '@coreui/react'
+import RegisterUserCard from '../../../components/admin/cards/RegisterUserCard'
+import UserListCard from '../../../components/admin/cards/UserListCard'
 
 const Usuarios = () => {
   return (
-    <>
-      <div>Usuarios</div>
-    </>
+    <CContainer>
+      <RegisterUserCard />
+      <UserListCard />
+    </CContainer>
   )
 }
 
